bf8033798484c831b2f35cade976c797eb13c54b
degzero <prajnithk@gmail.com>
2022-01-30 04:56:45 -0600

    Add files via upload

--- examples/scrolling_text.py
+++ examples/scrolling_text.py
@@ -1,61 +1,82 @@
-# MIT License
-
 # Copyright (c) 2014 Adafruit Industries
 # Author: Tony DiCola
-# Improved by the yongdol503
-
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
-import os.path
 import time
+
 import ST7735 as TFT
 import Adafruit_GPIO as GPIO
 import Adafruit_GPIO.SPI as SPI
-from PIL import Image, ImageDraw, ImageFont, ImageColor
 
-def main():
-  
-    color = ImageColor.getrgb('hsl(192, 100%, 84%)')
-    base = Image.new('RGBA', device.size, color)
-    
-    txt = Image.new('RGBA', base.size, (255,255,255,0))
 
-    font_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-        'fonts', 'NanumSquareRoundEB.ttf'))
+WIDTH = 128
+HEIGHT = 160
+SPEED_HZ = 16000000
 
-    fnt = ImageFont.truetype(font_path, device.height-70)
-    
+MESSAGE = "Hello World! How are you today?"
 
-    d = ImageDraw.Draw(txt)                         
 
-    d.text((10,30), "Yo", font=fnt, fill=(0,0,0,255))
-    
+# Raspberry Pi configuration.
+DC = 24
+RST = 25
+SPI_PORT = 0
+SPI_DEVICE = 0
 
-    out = Image.alpha_composite(base, txt)
-    
-    device.display(out.convert(device.mode))
+# BeagleBone Black configuration.
+# DC = 'P9_15'
+# RST = 'P9_12'
+# SPI_PORT = 1
+# SPI_DEVICE = 0
 
-if __name__ == '__main__':
-    try:
-        device=TFT.ST7735(rst=24, dc=25, rotate=180, 
-            spi=SPI.SpiDev(port=0,device=0,max_speed_hz=64*1000000))
-        device.begin()
-        main()
-    except KeyboardInterrupt:
-        pass
+# Create TFT LCD display class.
+disp = TFT.ST7735(
+    DC,
+    rst=RST,
+    spi=SPI.SpiDev(
+        SPI_PORT,
+        SPI_DEVICE,
+        max_speed_hz=SPEED_HZ))
+
+# Initialize display.
+disp.begin()
+
+
+img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+
+draw = ImageDraw.Draw(img)
+
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 40)
+
+size_x, size_y = draw.textsize(MESSAGE, font)
+
+text_x = 160
+text_y = (80 - size_y) // 2
+
+t_start = time.time()
+
+while True:
+    x = (time.time() - t_start) * 100
+    x %= (size_x + 160)
+    draw.rectangle((0, 0, 128, 160), (128, 128, 128))
+    draw.text((int(text_x - x), text_y), MESSAGE, font=font, fill=(255, 255, 255))
+    disp.display(img)
